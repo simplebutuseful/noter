37e7ca273f911850147c8dff21f376fa1929ad5c
simplebutuseful <simplebutuseful@interia.pl>
2023-12-10 22:09:03 +0100

    installation fixes
    Signed-off-by: simplebutuseful <simplebutuseful@interia.pl>

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 window.onload = function()
 {
+    'use strict';
     if ("serviceWorker" in navigator)
     {
+        alert("registering service worker");
         navigator.serviceWorker.register('./sw.js');
     };
 };
 
 var notenum=1;
-function addNote(){
+function addNote()
+{
     var category = document.getElementById("note-category").value;
     var text = document.getElementById("note-text").value;
     document.cookie = "note"+notenum+"="+category+text;
