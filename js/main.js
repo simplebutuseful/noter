0537cd63654aa98c74c7d2ef3257ee22075ce7a0
simplebutuseful <simplebutuseful@interia.pl>
2023-12-11 14:17:09 +0100

    spike solution - still ugly but works
    Signed-off-by: simplebutuseful <simplebutuseful@interia.pl>

--- js/main.js
+++ js/main.js
@@ -3,18 +3,78 @@ window.onload = function()
     'use strict';
     if ("serviceWorker" in navigator)
     {
-        alert("registering service worker");
         navigator.serviceWorker.register('/noter/sw.js');
     };
 };
 
+//class containing a single note entry
+class Note
+{
+    constructor(number,type,text)
+    {
+        this.number=number;
+        this.type=type;
+        this.text=text;
+    };
+    getName()
+    {
+        var divname="note"+this.number;
+        return divname;
+    };
+    show()
+    {
+        var divname=this.getName();
+        var divstart="<div id=\""+divname+"\">";
+        var notetitle="Note "+this.number+" ";
+        var notetype="Type: "+this.type+"<br>";
+        var notetext=" "+this.text+" ";
+        var divend="</div>";
+        var obj = document.getElementById("notes-printed");
+    var button = "<div><button type=\"button\" onclick=\"fulfillNote("+this.number+")\">check</button></div>";
+    obj.innerHTML+=
+            divstart
+            +notetitle
+            +notetype
+            +notetext
+            +divend
+        +button;
+    };
+    done()
+    {
+        var divname=this.getName();
+        var obj = document.getElementById(divname);
+    obj.classList.toggle("done");
+    };
+};
+
+//manage all notes
 var notenum=1;
+AllNotes = [];
+function fulfillNote(arg)
+{
+    var i=Number(arg)-1;
+    AllNotes[i].done();
+};
+function printNotes(Notes)
+{
+    document.getElementById("notes-printed").innerHTML="";
+    for(var i=(Notes.length - 1); i>=0 ;i--){
+        AllNotes[i].show();
+    }
+}
 function addNote()
 {
+    //add new note
     var category = document.getElementById("note-category").value;
     var text = document.getElementById("note-text").value;
-    document.cookie = "note"+notenum+"="+category+text;
-    var data = decodeURIComponent(document.cookie);
-    alert(data);
+    tmp = new Note(notenum,category,text);
+    AllNotes.push(tmp);
+
+    //print all notes
+    printNotes(AllNotes);
+
+    //document.cookie = "note"+notenum+"="+category+text;
+    //var data = decodeURIComponent(document.cookie);
+    //alert(data);
     notenum++;
 }
--- sw.js
+++ sw.js
@@ -1,35 +1,33 @@
 var cacheName = 'noter';
 var filesList = [ '/noter/', '/noter/index.html', '/noter/css/style.css', '/noter/js/main.js' ];
 
-// install the app by catching all files
+// install the app by caching all files
 self.addEventListener('install', 
-	function(eventInstance)
-	{
-	alert("adding install event");
-	eventInstance.waitUntil(
-	caches.open(cacheName).then(
-	    function(cacheInstance)
-	    {
-	    return cacheInstance.addAll(filesList);
-	    }
-	    )
-	);
-	self.skipWaiting();
-	}
+    function(eventInstance)
+    {
+    eventInstance.waitUntil(
+    caches.open(cacheName).then(
+        function(cacheInstance)
+        {
+        return cacheInstance.addAll(filesList);
+        }
+        )
+    );
+    self.skipWaiting();
+    }
 );
 
 // if cannot fetch files, use locals
 self.addEventListener('fetch',
-	function(eventInstance)
-	{
-alert("adding fetch event");
-	eventInstance.respondWith(
-	    caches.match(eventInstance.request).then(
-		function(response) {
-		return response || fetch(eventInstance.request);
-		}
-	    )
-	);
-	}
+    function(eventInstance)
+    {
+    eventInstance.respondWith(
+        caches.match(eventInstance.request).then(
+        function(response) {
+        return response || fetch(eventInstance.request);
+        }
+        )
+    );
+    }
 );
 
