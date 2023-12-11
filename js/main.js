b737ff8465543a22570f6aa25d272a40e07c729b
simplebutuseful <simplebutuseful@interia.pl>
2023-12-11 17:12:43 +0100

    styling and ui fixes
    Signed-off-by: simplebutuseful <simplebutuseful@interia.pl>

--- js/main.js
+++ js/main.js
@@ -15,35 +15,44 @@ class Note
         this.number=number;
         this.type=type;
         this.text=text;
+        this.isdone=false;
     };
     getName()
     {
         var divname="note"+this.number;
         return divname;
     };
+    shade()
+    {
+        if( this.isdone==true){
+            var divname=this.getName();
+            var obj = document.getElementById(divname);
+            obj.className="done";
+        };
+        if( this.isdone==false){
+            var divname=this.getName();
+            var obj = document.getElementById(divname);
+            obj.className="";
+        };
+    };
     show()
     {
         var divname=this.getName();
         var divstart="<div id=\""+divname+"\">";
         var notetitle="Note "+this.number+" ";
-        var notetype="Type: "+this.type+"<br>";
+        var notetype=""+this.type+"<br>";
         var notetext=" "+this.text+" ";
         var divend="</div>";
         var obj = document.getElementById("notes-printed");
-    var button = "<div><button type=\"button\" onclick=\"fulfillNote("+this.number+")\">check</button></div>";
-    obj.innerHTML+=
+        var button = "<div><button type=\"button\" onclick=\"fulfillNote("+this.number+")\">check</button></div>";
+        obj.innerHTML+=
             divstart
-            +notetitle
+            //+notetitle
             +notetype
             +notetext
             +divend
-        +button;
-    };
-    done()
-    {
-        var divname=this.getName();
-        var obj = document.getElementById(divname);
-    obj.classList.toggle("done");
+            +button;
+    this.shade();
     };
 };
 
@@ -53,8 +62,11 @@ AllNotes = [];
 function fulfillNote(arg)
 {
     var i=Number(arg)-1;
-    AllNotes[i].done();
+    AllNotes[i].isdone=!(AllNotes[i].isdone);
+    AllNotes[i].shade();
 };
+
+//print all notes in the document
 function printNotes(Notes)
 {
     document.getElementById("notes-printed").innerHTML="";
@@ -76,5 +88,6 @@ function addNote()
     //document.cookie = "note"+notenum+"="+category+text;
     //var data = decodeURIComponent(document.cookie);
     //alert(data);
+    document.getElementById("note-text").value="";
     notenum++;
 }
